cc46c04e209c9851536477603c8fda3a269b123d
karenok <karenokonkwo29@gmail.com>
2019-11-15 11:39:27 +0000

    Changed logout path from relative to absolute

--- process/logout.php
+++ process/logout.php
@@ -1,14 +1,13 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Unset all of the session variables
 $_SESSION = array();
- 
+
 // Destroy the session.
 session_destroy();
- 
+
 // Redirect to login page
-header("location: ../views/login.php");
+header("location:" . $_SERVER['DOCUMENT_ROOT'] . "/../../../" . "/cbe-software/views/login.php");
 exit;
-?>
