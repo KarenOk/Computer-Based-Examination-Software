4012761cd14af3ff54c4022d23cb41f9c9711309
Karen Okonkwo <karenokonkwo29@gmail.com>
2020-11-17 14:20:09 +0100

    Fix: redirect to wrong login path

--- helpers/logout.php
+++ helpers/logout.php
@@ -9,5 +9,5 @@ $_SESSION = array();
 session_destroy();
 
 // Redirect to login page
-header("location:" . $_SERVER['DOCUMENT_ROOT'] . "/../../../" . "/cbe-software/views/login.php");
+header("location:" . $_SERVER['DOCUMENT_ROOT'] . "/../../../" . "views/login.php");
 exit;
